de934f10ddfeaebc6e95d7ff38e57c9c1818e28d
pomehy <hlopkov.gleb@mail.ru>
2021-11-12 18:08:11 +0300

    подключает к main.js preloader();

--- source/js/main.js
+++ source/js/main.js
@@ -2,12 +2,13 @@ import {ieFix} from './utils/ie-fix';
 import {iosVhFix} from './utils/ios-vh-fix';
 
 import {initModals} from './modules/init-modals';
-
+import {preloader} from './modules/preloader';
 // Utils
 // ---------------------------------
 
 ieFix();
 iosVhFix();
+preloader();
 
 // Modules
 // ---------------------------------
